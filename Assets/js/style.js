daff6c6397a7f0bc223434ef632589de16aaa3e0
Rafet Mammadov <professionalraf@gmail.com>
2021-11-03 16:35:15 +0400

    first section end

--- Assets/js/style.js
+++ Assets/js/style.js
@@ -1,3 +1,26 @@
+$('.slider-owl').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        992:{
+            items:4
+        },
+        1146:{
+            items:5
+        }
+    }
+});
+
+
+
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -14,4 +37,17 @@ $('.owl-carousel').owlCarousel({
             items:1
         }
     }
-})
+});
+
+function myFunction(){
+    var element=document.getElementById('slide-component');
+    element.classList.toggle('style-1')
+}
+function myFunction1(){
+    var element1 =document.getElementById('none-opacity');
+    element1.classList.toggle('style-2')
+}
+function myFunction2(){
+    var element2 =document.getElementById('slide-component-2');
+    element2.classList.toggle('style-3')
+}
