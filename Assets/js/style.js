4ffd97fb1c1af1ae21dfbc14bbf8b993370eed88
Rafet Mammadov <professionalraf@gmail.com>
2021-11-06 00:42:05 +0400

    fashion section end

--- Assets/js/style.js
+++ Assets/js/style.js
@@ -1,3 +1,26 @@
+$('.fashion-slide').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+
+
+
+
+
 $('.today-slider').owlCarousel({
     loop:true,
     margin:10,
