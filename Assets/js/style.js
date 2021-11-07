ec9a7d624060301190b81e9fb3f84270a65ac492
Rafet Mammadov <professionalraf@gmail.com>
2021-11-07 22:31:16 +0400

    footer started

--- Assets/js/style.js
+++ Assets/js/style.js
@@ -1,3 +1,28 @@
+$('.from-our-blog-slide').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+
+
+
+
+
+
+
 $('.today-slide').owlCarousel({
     loop:true,
     margin:10,
